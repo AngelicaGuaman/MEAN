1ecabe3be2c20a931be2a9f4323906981009351a
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2019-08-07 17:21:23 +0200

    created root child

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { PagesComponent } from './pages.component';
+import { PAGES_ROUTES } from './pages.routes';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { PagesComponent } from './pages.component';
     Graficas1Component
   ],
   imports: [
-    SharedModule
+    SharedModule,
+    PAGES_ROUTES
   ]
 })
 export class PagesModule { }
